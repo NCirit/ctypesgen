88e690284c835eab4511bb4dd925d43a49a21464
ncirit <n.cirit.n@gmail.com>
2024-01-25 18:00:33 +0300

    Handle static library, selection of compiler, and etc
    Ctypesgen doesn't support static libraries. If user passes static
    library, automaticaly create a shared library for it and use it for
    python bindings.
    
    By default the compiler for preprocessing is set to CMAKE_CXX_COMPILER
    but if the target uses CMAKE_C_COMPILER it will set to that.
    
    Adding ctypesgen_def.h header file for defining things. It is useful for
    handling things that ctypesgen doesn't support. For example msvc has
    built-in sized arguments named __int64 and so. When ctypes gen
    encounters any of symbol which uses them it yields with error and skips
    creating python bindings for it.

--- cmake/ctypesgen.cmake
+++ cmake/ctypesgen.cmake
@@ -1,3 +1,4 @@
+set(CTYPESGEN_INTERNAL_DIR ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "")
 
 function(ctype_python_binding targets)
     # Check if ctypesgen module exists
@@ -13,21 +14,32 @@ function(ctype_python_binding targets)
 
     # Iterate over all targets and add targets for python bindings
     foreach(target ${targets})
+        set(shared_library_name ${target})
+        get_target_property(target_type ${target} TYPE)
+        if (target_type STREQUAL STATIC_LIBRARY)
+        message(STATUS "Given ${target} library is static, ctypesgen supports shared librarys only so ${target}_ctypesgen_shared target is created for to create bindings")
+        add_library(${target}_ctypesgen_shared SHARED $<TARGET_OBJECTS:${target}>)
+        target_link_libraries(${target}_ctypesgen_shared PUBLIC ${target})
+        set(shared_library_name ${target}_ctypesgen_shared)
+        endif()
+
+
         # Enable preprocessed outputs for headers
+        list(APPEND myList "value1" "\"value with spaces\"" "value3")
         if(MSVC)
-            set_property(TARGET ${target} PROPERTY WINDOWS_EXPORT_ALL_SYMBOLS ON)
-            set(compiler_args /P /EP /Fi${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates/)
+            set_property(TARGET ${shared_library_name} PROPERTY WINDOWS_EXPORT_ALL_SYMBOLS ON)
+            list(APPEND compiler_args /P /EP /Fi${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates/ /FI${CTYPESGEN_INTERNAL_DIR}/ctypesgen_defs.h)
         else()
-            set(compiler_args -E -P -o ${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates/)
+            list(APPEND compiler_args -E -P -o ${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates/ -dD "-D__extension__==" "-D__const=const" "-D__asm__(x)=" "-D__asm(x)=" "-DCTYPESGEN=1")
         endif()
 
         get_target_property(include_dirs ${target} INTERFACE_INCLUDE_DIRECTORIES)
         if(include_dirs)
-            foreach(include_dirs ${public_headers})
+            foreach(include_dir ${include_dirs})
                 if(MSVC)
-                    set(compiler_args ${compiler_args} "/I${public_header}")
+                list(APPEND compiler_args "/I${include_dir}")
                 else()
-                    set(compiler_args ${compiler_args} "-I${public_header}")
+                list(APPEND compiler_args "-I${include_dir}")
                 endif()
             endforeach()
         endif()
@@ -36,9 +48,9 @@ function(ctype_python_binding targets)
         if(compile_defs)
             foreach(comp_definition ${compile_defs})
             if(MSVC)
-            set(compiler_args ${compiler_args} /D${comp_definition})
+            list(APPEND compiler_args /D${comp_definition})
             else()
-            set(compiler_args ${compiler_args} -D${comp_definition})
+            list(APPEND compiler_args -D${comp_definition})
             endif()
             endforeach()
         endif()
@@ -54,10 +66,17 @@ function(ctype_python_binding targets)
 
         set(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${HEADER}.i")
 
-        add_custom_command(
-            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates
+        set(compiler ${CMAKE_CXX_COMPILER})
+
+        get_target_property(target_language ${target} LANGUAGE)
+        if("${target_language}" STREQUAL "C")
+        set(compiler ${CMAKE_C_COMPILER})
+        endif()
+
+        string(JOIN " " compiler_args "${compiler_args}")
+        add_custom_target(${target}_preprocessed_headers
             COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates/
-            COMMAND ${CMAKE_CXX_COMPILER} ${compiler_args} ${public_headers}
+            COMMAND ${compiler} ${compiler_args} ${public_headers}
             COMMENT "Preprocessing headers ${public_headers} "
             DEPENDS ${target}
             VERBATIM
@@ -73,8 +92,8 @@ function(ctype_python_binding targets)
         if(NOT "${header_inputs}" EQUAL "")
             message("${header_inputs}")
             add_custom_target(${target}.py 
-                COMMAND python -m ctypesgen --preprocessed --all-headers -l$<TARGET_FILE:${target}> ${preprocessed_headers} -o ${target}.py
-            DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/ctypesgen_intermediates
+                COMMAND python -m ctypesgen --preprocessed --all-headers --save-preprocessed-headers ${target}_preprocessed.i -l$<TARGET_FILE:${shared_library_name}> ${preprocessed_headers} -o ${target}.py
+            DEPENDS ${target}_preprocessed_headers
             )
         endif()
 
---
+++ cmake/ctypesgen_defs.h
@@ -0,0 +1,8 @@
+
+#define __extension__
+#define __const const
+#define __asm__(x)
+#define CTYPESGEN 1
+#define __declspec(x)
+
+#define __int64 long long
